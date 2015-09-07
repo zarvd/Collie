b82f174fce14ffab694071ac7ec05626d7612603
GallonChoi <404842441@qq.com>
2015-09-07 17:17:28 +0000

    init http header

--- http_serv.cpp
+++ http_serv.cpp
@@ -2,6 +2,56 @@
 
 
 namespace Http {
+    const std::map<std::string, std::string> MimeType = {
+        {"html", "text/html"},
+        {"htm", "text/html"},
+        {"shtm", "text/html"},
+        {"shtml", "text/html"},
+        {"css", "text/css"},
+        {"js",  "application/javascript"},
+        {"ico", "image/x-icon"},
+        {"gif", "image/gif"},
+        {"jpg", "image/jpeg"},
+        {"jpeg", "image/jpeg"},
+        {"png", "image/png"},
+        {"svg", "image/svg+xml"},
+        {"txt", "text/plain"},
+        {"torrent", "application/x-bittorrent"},
+        {"wav", "audio/x-wav"},
+        {"mp3", "audio/x-mp3"},
+        {"mid", "audio/mid"},
+        {"m3u", "audio/x-mpegurl"},
+        {"ogg", "application/ogg"},
+        {"ram", "audio/x-pn-realaudio"},
+        {"xml", "text/xml"},
+        {"json",  "application/json"},
+        {"xslt", "application/xml"},
+        {"xsl", "application/xml"},
+        {"ra",  "audio/x-pn-realaudio"},
+        {"doc", "application/msword"},
+        {"exe", "application/octet-stream"},
+        {"zip", "application/x-zip-compressed"},
+        {"xls", "application/excel"},
+        {"tgz", "application/x-tar-gz"},
+        {"tar", "application/x-tar"},
+        {"gz",  "application/x-gunzip"},
+        {"arj", "application/x-arj-compressed"},
+        {"rar", "application/x-rar-compressed"},
+        {"rtf", "application/rtf"},
+        {"pdf", "application/pdf"},
+        {"swf", "application/x-shockwave-flash"},
+        {"mpg", "video/mpeg"},
+        {"webm", "video/webm"},
+        {"mpeg", "video/mpeg"},
+        {"mov", "video/quicktime"},
+        {"mp4", "video/mp4"},
+        {"m4v", "video/x-m4v"},
+        {"asf", "video/x-ms-asf"},
+        {"avi", "video/x-msvideo"},
+        {"bmp", "image/bmp"},
+        {"ttf", "application/x-font-ttf"}
+    };
+
     std::string getStateByCode(const unsigned short& code) {
         switch(code) {
         case 100: return "Continue";
--- http_serv.hpp
+++ http_serv.hpp
@@ -8,68 +8,29 @@
 
 namespace Http {
 
-    const std::map<const std::string, const std::string> MimeType = {
-        {"html", "text/html"},
-        {"htm", "text/html"},
-        {"shtm", "text/html"},
-        {"shtml", "text/html"},
-        {"css", "text/css"},
-        {"js",  "application/javascript"},
-        {"ico", "image/x-icon"},
-        {"gif", "image/gif"},
-        {"jpg", "image/jpeg"},
-        {"jpeg", "image/jpeg"},
-        {"png", "image/png"},
-        {"svg", "image/svg+xml"},
-        {"txt", "text/plain"},
-        {"torrent", "application/x-bittorrent"},
-        {"wav", "audio/x-wav"},
-        {"mp3", "audio/x-mp3"},
-        {"mid", "audio/mid"},
-        {"m3u", "audio/x-mpegurl"},
-        {"ogg", "application/ogg"},
-        {"ram", "audio/x-pn-realaudio"},
-        {"xml", "text/xml"},
-        {"json",  "application/json"},
-        {"xslt", "application/xml"},
-        {"xsl", "application/xml"},
-        {"ra",  "audio/x-pn-realaudio"},
-        {"doc", "application/msword"},
-        {"exe", "application/octet-stream"},
-        {"zip", "application/x-zip-compressed"},
-        {"xls", "application/excel"},
-        {"tgz", "application/x-tar-gz"},
-        {"tar", "application/x-tar"},
-        {"gz",  "application/x-gunzip"},
-        {"arj", "application/x-arj-compressed"},
-        {"rar", "application/x-rar-compressed"},
-        {"rtf", "application/rtf"},
-        {"pdf", "application/pdf"},
-        {"swf", "application/x-shockwave-flash"},
-        {"mpg", "video/mpeg"},
-        {"webm", "video/webm"},
-        {"mpeg", "video/mpeg"},
-        {"mov", "video/quicktime"},
-        {"mp4", "video/mp4"},
-        {"m4v", "video/x-m4v"},
-        {"asf", "video/x-ms-asf"},
-        {"avi", "video/x-msvideo"},
-        {"bmp", "image/bmp"},
-        {"ttf", "application/x-font-ttf"}
+    extern const std::map<std::string, std::string> MimeType;
+
+    class HttpHeader {
+    protected:
+        std::map<std::string, bool> selectItem;
+        std::map<std::string, std::string> item;
+    public:
+        HttpHeader() = delete;
+        virtual Status setItemValue(const std::string&, const std::string&);
+        virtual Status assignItem(const std::string&, const bool&);
     };
 
-    struct GeneralHeader {
-    };
-    struct RequestHeader {
-    };
-    struct ResponseHeader {
-    };
-    struct EntityHeader {
-    };
+    class GeneralHeader final : public HttpHeader {};
+
+    class RequestHeader final : public HttpHeader {};
+
+    class ResponseHeader final : public HttpHeader {};
+
+    class EntityHeader final : public HttpHeader {};
 
     std::string getStateByCode(const unsigned short&);
     std::string getMimeTypeByExt(const std::string&);
-    std::string createHeader();
+    std::string createHeader(GeneralHeader&, RequestHeader&, ResponseHeader&, EntityHeader&);
     void start();
 }
 
