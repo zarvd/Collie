81b464ad3efa093069b06bde6cae6971ad4ba70c
GallonChoi <404842441@qq.com>
2015-10-11 17:49:11 +0000

    Add callback

---
+++ base/Callback.hpp
@@ -0,0 +1,14 @@
+#ifndef MINIHTTP_BASE_CALLBACK_HPP
+#define MINIHTTP_BASE_CALLBACK_HPP
+
+#include <functional>
+
+
+namespace MiniHttp { namespace Base {
+
+typedef std::function<void(const unsigned &)> ConnectCallback;
+typedef std::function<void(const unsigned &)> EventCallback;
+
+}}
+
+#endif /* MINIHTTP_BASE_CALLBACK_HPP */
