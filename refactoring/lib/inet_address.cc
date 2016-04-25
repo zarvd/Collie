7ab01e85c8a21023e4ecb048eb45990afe137204
GallonChoi <gallonq@gmail.com>
2016-04-25 22:03:47 +0000

    fixes parsing ip

--- refactoring/lib/inet_address.cc
+++ refactoring/lib/inet_address.cc
@@ -1,6 +1,7 @@
 #include <netdb.h>
 #include <string.h>
 #include "../inc/inet_address.h"
+#include <iostream>
 
 namespace collie {
 
@@ -15,7 +16,7 @@ std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
                              ::gai_strerror(err_code));
   }
 
-  char hostname[100];
+  char hostname[NI_MAXHOST];
   err_code = getnameinfo(addr_info->ai_addr, addr_info->ai_addrlen, hostname,
                          NI_MAXHOST, NULL, 0, 0);
   if (err_code != 0) {
@@ -23,46 +24,37 @@ std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
                              ::gai_strerror(err_code));
   }
 
-  const auto ip_family = addr_info->ai_family;
-
-  ::freeaddrinfo(addr_info);
-
   std::shared_ptr<InetAddress> inet_address = nullptr;
 
-  if (ip_family == AF_INET) {
+  if (addr_info->ai_family == AF_INET) {
     inet_address = std::make_shared<IPv4Address>();
-
-    inet_address->host_ = hostname;
-    inet_address->port_ = port;
-    sockaddr_in *addr = new sockaddr_in;
-    addr->sin_family = AF_INET;
-    addr->sin_port = ::htons(port);
-    if (::inet_pton(AF_INET, hostname, &(addr->sin_addr)) == -1) {
-      delete addr;
+    char host_address[INET_ADDRSTRLEN];
+    sockaddr_in *addr = ((sockaddr_in *)addr_info->ai_addr);
+    if (!inet_ntop(AF_INET, &addr->sin_addr, host_address, INET_ADDRSTRLEN)) {
       throw std::runtime_error(std::string("Cannot parse host: ") +
                                ::strerror(errno));
     }
-    inet_address->address_ = Address(addr);
+    inet_address->host_ = host_address;
 
-  } else if (ip_family == AF_INET6) {
+  } else if (addr_info->ai_family == AF_INET6) {
     inet_address = std::make_shared<IPv6Address>();
-
-    inet_address->host_ = hostname;
-    inet_address->port_ = port;
-
-    sockaddr_in6 *addr = new sockaddr_in6;
-    addr->sin6_family = AF_INET6;
-    addr->sin6_port = ::htons(port);
-    if (::inet_pton(AF_INET6, hostname, &(addr->sin6_addr)) == -1) {
-      delete addr;
+    char host_address[INET6_ADDRSTRLEN];
+    sockaddr_in6 *addr = ((sockaddr_in6 *)addr_info->ai_addr);
+    if (!inet_ntop(AF_INET6, &addr->sin6_addr, host_address,
+                   INET6_ADDRSTRLEN)) {
       throw std::runtime_error(std::string("Cannot parse host: ") +
                                ::strerror(errno));
     }
-    inet_address->address_ = Address(addr);
+    inet_address->host_ = host_address;
 
   } else {
+    ::freeaddrinfo(addr_info);
     throw std::runtime_error("Cannot parse host: Unknown family");
   }
+  inet_address->port_ = port;
+  inet_address->address_ = addr_info->ai_addr;
+
+  ::freeaddrinfo(addr_info);
 
   return inet_address;
 }
--- refactoring/test/test_inet_address.cc
+++ refactoring/test/test_inet_address.cc
@@ -5,18 +5,22 @@ int main(void) {
   auto address1 = collie::InetAddress::GetInetAddress("192.168.199.136", 8080);
   auto address2 =
       collie::InetAddress::GetInetAddress("fe80::260a:64ff:fe38:2cb4", 8080);
-  // auto address = collie::InetAddress::GetInetAddress("localhost", 8080);
-  auto address = collie::InetAddress::GetInetAddress("bilibili.com", 8080);
-  if (address)
-    std::cout << "host : " << address->host() << std::endl
-              << "port : " << address->port() << std::endl;
-
+  // auto address3 = collie::InetAddress::GetInetAddress("localhost", 8080);
+  // auto address3 = collie::InetAddress::GetInetAddress("bilibili.com", 8080);
+  // auto address4 =
+  //     collie::InetAddress::GetInetAddress("http://ipv6.sjtu.edu.cn/", 8080);
   if (address1)
     std::cout << "host : " << address1->host() << std::endl
               << "port : " << address1->port() << std::endl;
   if (address2)
     std::cout << "host : " << address2->host() << std::endl
               << "port : " << address2->port() << std::endl;
+  // if (address3)
+  //   std::cout << "host : " << address3->host() << std::endl
+  //             << "port : " << address3->port() << std::endl;
+  // if (address4)
+  //   std::cout << "host : " << address4->host() << std::endl
+  //             << "port : " << address4->port() << std::endl;
 
   return 0;
 }
