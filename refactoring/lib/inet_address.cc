4a76f2be3dd176f419fe6a2d05cbc2d5f453269f
GallonChoi <gallonq@gmail.com>
2016-04-26 08:43:20 +0000

    fixes parsing address

--- refactoring/lib/inet_address.cc
+++ refactoring/lib/inet_address.cc
@@ -1,7 +1,6 @@
 #include <netdb.h>
 #include <string.h>
 #include "../inc/inet_address.h"
-#include <iostream>
 
 namespace collie {
 
@@ -9,16 +8,9 @@ InetAddress::~InetAddress() noexcept {}
 
 std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
     const Host &host, const Port port) throw(std::runtime_error) {
+  // Gets address info
   addrinfo *addr_info;
   int err_code = ::getaddrinfo(host.c_str(), nullptr, nullptr, &addr_info);
-  if (err_code != 0) {
-    throw std::runtime_error(std::string("Cannot parse host: ") +
-                             ::gai_strerror(err_code));
-  }
-
-  char hostname[NI_MAXHOST];
-  err_code = getnameinfo(addr_info->ai_addr, addr_info->ai_addrlen, hostname,
-                         NI_MAXHOST, NULL, 0, 0);
   if (err_code != 0) {
     throw std::runtime_error(std::string("Cannot parse host: ") +
                              ::gai_strerror(err_code));
@@ -27,9 +19,11 @@ std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
   std::shared_ptr<InetAddress> inet_address = nullptr;
 
   if (addr_info->ai_family == AF_INET) {
+    // IPv4
     inet_address = std::make_shared<IPv4Address>();
     char host_address[INET_ADDRSTRLEN];
     sockaddr_in *addr = ((sockaddr_in *)addr_info->ai_addr);
+    addr->sin_port = ::htons(port);
     if (!inet_ntop(AF_INET, &addr->sin_addr, host_address, INET_ADDRSTRLEN)) {
       throw std::runtime_error(std::string("Cannot parse host: ") +
                                ::strerror(errno));
@@ -37,9 +31,11 @@ std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
     inet_address->host_ = host_address;
 
   } else if (addr_info->ai_family == AF_INET6) {
+    // IPv6
     inet_address = std::make_shared<IPv6Address>();
     char host_address[INET6_ADDRSTRLEN];
     sockaddr_in6 *addr = ((sockaddr_in6 *)addr_info->ai_addr);
+    addr->sin6_port = ::htons(port);
     if (!inet_ntop(AF_INET6, &addr->sin6_addr, host_address,
                    INET6_ADDRSTRLEN)) {
       throw std::runtime_error(std::string("Cannot parse host: ") +
@@ -53,7 +49,6 @@ std::shared_ptr<InetAddress> InetAddress::GetInetAddress(
   }
   inet_address->port_ = port;
   inet_address->address_ = addr_info->ai_addr;
-
   ::freeaddrinfo(addr_info);
 
   return inet_address;
--- refactoring/test/test_inet_address.cc
+++ refactoring/test/test_inet_address.cc
@@ -5,22 +5,20 @@ int main(void) {
   auto address1 = collie::InetAddress::GetInetAddress("192.168.199.136", 8080);
   auto address2 =
       collie::InetAddress::GetInetAddress("fe80::260a:64ff:fe38:2cb4", 8080);
-  // auto address3 = collie::InetAddress::GetInetAddress("localhost", 8080);
-  // auto address3 = collie::InetAddress::GetInetAddress("bilibili.com", 8080);
-  // auto address4 =
-  //     collie::InetAddress::GetInetAddress("http://ipv6.sjtu.edu.cn/", 8080);
+  auto address3 = collie::InetAddress::GetInetAddress("bilibili.com", 8080);
+  auto address4 = collie::InetAddress::GetInetAddress("localhost", 8080);
   if (address1)
     std::cout << "host : " << address1->host() << std::endl
               << "port : " << address1->port() << std::endl;
   if (address2)
     std::cout << "host : " << address2->host() << std::endl
               << "port : " << address2->port() << std::endl;
-  // if (address3)
-  //   std::cout << "host : " << address3->host() << std::endl
-  //             << "port : " << address3->port() << std::endl;
-  // if (address4)
-  //   std::cout << "host : " << address4->host() << std::endl
-  //             << "port : " << address4->port() << std::endl;
+  if (address3)
+    std::cout << "host : " << address3->host() << std::endl
+              << "port : " << address3->port() << std::endl;
+  if (address4)
+    std::cout << "host : " << address4->host() << std::endl
+              << "port : " << address4->port() << std::endl;
 
   return 0;
 }
